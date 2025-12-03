b59760fa27a47487fe4668164a9fb782af68eba9
Pau B. <103254266+paubcdev@users.noreply.github.com>
2025-12-03 10:47:06 +0100

    removed comments, unnecesary

--- days/day03/day03.go
+++ days/day03/day03.go
@@ -38,10 +38,8 @@ func (s *Solution) Run(part int) {
 func findMaxJoltage(bank string) int64 {
 	maxJoltage := int64(0)
 
-	// Try all pairs of positions
 	for i := 0; i < len(bank); i++ {
 		for j := i + 1; j < len(bank); j++ {
-			// Form the number from batteries at positions i and j
 			joltageStr := string(bank[i]) + string(bank[j])
 			joltage, err := strconv.ParseInt(joltageStr, 10, 64)
 			if err != nil {
@@ -63,21 +61,14 @@ func findMaxJoltagePart2(bank string) int64 {
 		return 0
 	}
 
-	// Greedy approach: to maximize a 12-digit number,
-	// at each position, pick the largest available digit
-	// We need to skip (n - 12) digits total
-
 	toSkip := n - 12
 	result := ""
 	i := 0
 
 	for len(result) < 12 && i < n {
-		// Look ahead to see the best digit we can pick
-		// We can skip at most (toSkip) more digits
 		maxDigit := byte('0')
 		maxPos := i
 
-		// Check the next (toSkip + 1) positions
 		for j := i; j <= i+toSkip && j < n; j++ {
 			if bank[j] > maxDigit {
 				maxDigit = bank[j]
@@ -85,9 +76,7 @@ func findMaxJoltagePart2(bank string) int64 {
 			}
 		}
 
-		// Pick the maximum digit found
 		result += string(maxDigit)
-		// Update how many we can skip and where we are
 		toSkip -= (maxPos - i)
 		i = maxPos + 1
 	}
